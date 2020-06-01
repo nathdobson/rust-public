a9f8ddb55827505b63e6607540e4eca8390d8385
Nathan Dobson <nadobson@mac.com>
2020-05-31 19:09:31 -0700

    add web server

---
+++ util/examples/web.rs
@@ -0,0 +1,5 @@
+use util::web::start_web_server;
+
+fn main(){
+    start_web_server("0.0.0.0:8000", "./examples/web.rs".as_ref()).unwrap();
+}
---
+++ util/src/web.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::net::{TcpListener, Shutdown};
+use std::io::Write;
+use std::path::Path;
+use std::thread;
+
+pub fn start_web_server(addr: &str, filename: &Path) -> io::Result<()> {
+    let listener = TcpListener::bind(addr)?;
+    let content = std::fs::read_to_string(filename)?;
+    let mut packet = Vec::new();
+
+    write!(&mut packet, "HTTP/1.0 200 OK\r\n")?;
+    write!(&mut packet, "Content-Length: {}\r\n", content.len())?;
+    write!(&mut packet, "Content-Type: application/octet-stream\r\n")?;
+    write!(&mut packet, "Content-Disposition: attachment; filename=\"{}\"\r\n",
+           filename.file_name().unwrap().to_str().unwrap())?;
+    write!(&mut packet, "\r\n")?;
+    write!(&mut packet, "{}", content)?;
+    thread::spawn(move || {
+        loop {
+            let (mut socket, _) = listener.accept().unwrap();
+            if let Err(e) = || -> io::Result<()>{
+                socket.set_nonblocking(true)?;
+                socket.write(&packet)?;
+                socket.shutdown(Shutdown::Write)?;
+                Ok(())
+            }() {
+                eprintln!("HTTP error: {:?}", e);
+            }
+        }
+    });
+    Ok(())
+}
