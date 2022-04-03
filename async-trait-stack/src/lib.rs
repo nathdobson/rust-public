d09b95c0512f970f144dea101ee74ed94e711af7
Nathan Dobson <nadobson@mac.com>
2022-04-03 08:49:43 -0700

    Update versions

---
+++ async-trait-stack/src/lib.rs
@@ -0,0 +1,16 @@
+#![feature(allocator_api)]
+
+use std::alloc::{Allocator, AllocError, Layout};
+use std::cell::{Cell, UnsafeCell};
+use std::mem::MaybeUninit;
+use std::ptr::NonNull;
+
+#[repr(align(16))]
+struct InlineAllocator<T> {
+    allocated: Cell<bool>,
+    value: UnsafeCell<MaybeUninit<T>>,
+}
+
+impl<T> InlineAllocator<T> {
+    pub fn new() -> Self { InlineAllocator { allocated: Cell::new(false), value: UnsafeCell::new(MaybeUninit::new()) } }
+}
